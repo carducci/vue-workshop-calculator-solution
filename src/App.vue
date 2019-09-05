466c1f4ceb1658b7d3bc8d2d07cb8bc898364a46
Carducci <michael@mago.co>
2019-09-05 08:06:53 -0500

    vue workshop lab 3 calculator solution complete

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <TheCalculator></TheCalculator>
+  </div>
+</template>
+
+<script>
+import TheCalculator from "./components/TheCalculator.vue";
+
+export default {
+  name: "app",
+  components: {
+    TheCalculator
+  }
+};
+</script>
+
+<style>
+html {
+  font-size: 62.5%;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: inherit;
+}
+</style>
